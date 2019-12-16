1c4a677222277ba277d0f35d066ef5bb549defc9
Emily <emily.a.harber@gmail.com>
2019-12-16 17:41:24 -0500

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,35 @@
 class GenresController < ApplicationController
+
+  def show
+    @genre = Genre.find_by(params[:id])
+  end
+
+  def new
+    @genre = Genre.new 
+  end
+
+  def create
+    @genre = Genre.new(get_genre_params)
+    @genre.save 
+
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find_by(params[:id])
+  end
+
+  def update
+    @genre = Genre.find_by(params[:id])
+    @genre.update(get_genre_params)
+
+    redirect_to genre_path(@genre)
+  end
+
+  private 
+
+    def get_genre_params
+      params.require(:genre).permit(:name)
+    end 
+  
 end
