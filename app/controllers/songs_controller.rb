91b1bfcb5adc2c17294dd139cf97128b3b33aec2
Emily <emily.a.harber@gmail.com>
2019-12-16 17:52:50 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,19 +1,38 @@
 class SongsController < ApplicationController
+  
   def index
-  end
+    @songs = Song.all 
+  end 
 
   def show
+    @song = Song.find_by(params[:id])
   end
 
   def new
+    @song = Song.new 
   end
 
   def create
+    @song = Song.new(get_song_params)
+    @song.save 
+
+    redirect_to song_path(@song)
   end
 
   def edit
+    @song = Song.find_by(params[:id])
   end
 
   def update
+    @song = Song.find_by(params[:id])
+    @song.update(get_song_params)
+
+    redirect_to song_path(@song)
   end
+
+  private 
+
+    def get_song_params
+      params.require(:song).permit(:name, :genre_id, :artist_id)
+    end 
 end
