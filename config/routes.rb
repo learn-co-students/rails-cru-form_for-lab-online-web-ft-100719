6a42245d271384800ff885410f19992b774046e4
github username <github email address>
2020-01-09 17:51:57 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs, only: [:index, :new, :show, :edit, :update, :create]
+  resources :genres, only:[:index, :new, :show, :edit, :update, :create]
+  resources :artists, only:[:index, :new, :show, :edit, :update, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
