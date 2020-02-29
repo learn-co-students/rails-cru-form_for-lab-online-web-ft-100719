1f0beb964e896fff38a6c6744376155a6aabdd01
jermain119 <jermain119@gmail.com>
2020-02-28 20:01:12 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,8 +6,7 @@ class SongsController < ApplicationController
 
 	def show
     @song = Song.find(params[:id])
-    @genre = Genre.find(@song.genre_id)
-    @artist = Artist.find(@song.artist_id)
+   
 	end
 
 	def new
